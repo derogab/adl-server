4da65cd86abe48aea543ab2b4cddb9094335c551
Gabriele De Rosa <derosagabriele@outlook.it>
2020-06-09 16:12:51 +0200

    Constants

---
+++ constants.py
@@ -0,0 +1,15 @@
+# Constants
+# Class w/ useful constants
+class Constants:
+    # connection
+    connection_max_timeout = 9999999
+    # sensor type
+    sensor_type_accelerometer = "accelerometer"
+    sensor_type_gyroscope = "gyroscope"
+    # request
+    request_status_success = "OK"
+    request_mode_analyzer = "analyzer"
+    request_mode_learning = "learning"
+    request_type_data = "data"
+    request_type_close = "close"
+
