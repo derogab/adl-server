e3fa07896d3d1c69251bf6645b466d553841630f
Gabriele De Rosa <derosagabriele@outlook.it>
2020-06-09 16:13:54 +0200

    Teacher

---
+++ teacher.py
@@ -0,0 +1,10 @@
+class Teacher():
+
+    def __init__(self):
+        print("Teacher created.")
+        pass
+
+    def collect(self, archive, index, activity, sensor, position, values):
+        pass
+    
+    pass
