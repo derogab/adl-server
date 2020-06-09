dbc0db5a39ff35715526a7f9d93954928551dea3
Gabriele De Rosa <derosagabriele@outlook.it>
2020-06-09 16:13:41 +0200

    Wizard

---
+++ wizard.py
@@ -0,0 +1,11 @@
+class Wizard():
+
+    def __init__(self):
+        print("Wizard created.")
+        pass
+
+    def collect(self, archive, index, sensor, position, values):
+
+        # @TODO: collect the data
+        
+        pass
